4c877fd6f3df1fe4979b963c6d274289ee08884d
imShubh-AL <shubham.waghmare@aimedlabs.com>
2023-07-18 13:09:21 +0530

    final vs static vs <some-data-type> vs var completed

--- Code Scope in Dart/b_accesing_private_methods.dart
+++
@@ -1,6 +0,0 @@
-import 'b_private_method_or_class.dart';
-
-void main() {
-  final instance = MyClass();
-  instance._privateMethod();
-}
---
+++ final_vs_static_vs_<some-data-type>_vs_var.dart
@@ -0,0 +1,7 @@
+class MyClass {
+  final int finalVariable = 10;
+  static int staticVariable = 20;
+  int someDataTypeVar = 30;
+  var simpleVariable =
+      'Hello'; //in this case the data type is inferred by compiler on the basis of that assinged values
+}
