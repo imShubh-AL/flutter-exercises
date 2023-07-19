cf7031fc8f77f8522d0c19e4e8bba85bea76420b
imShubh-AL <shubham.waghmare@aimedlabs.com>
2023-07-19 11:17:40 +0530

    Extensions in dart completed

--- final_vs_static_vs_<some-data-type>_vs_var.dart
+++
@@ -1,7 +0,0 @@
-class MyClass {
-  final int finalVariable = 10;
-  static int staticVariable = 20;
-  int someDataTypeVar = 30;
-  var simpleVariable =
-      'Hello'; //in this case the data type is inferred by compiler on the basis of that assinged values
-}
